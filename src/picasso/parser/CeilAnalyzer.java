4610ea59c6ef6c5d28dd1768d723b2d28a60c3fd
choec23 <choec23@mail.wlu.edu>
2021-11-28 14:42:15 -0500

    Implemented Ceil function

---
+++ src/picasso/parser/CeilAnalyzer.java
@@ -0,0 +1,27 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Ceil;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the ceil function
+ * 
+ * @author calebchoe
+ *
+ */
+
+public class CeilAnalyzer extends UnaryFunctionAnalyzer {
+
+
+		@Override
+		public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+			tokens.pop(); // Need to remove the floor token
+			// the parameter is the next token on the stack.
+			// But, it needs to be processed
+			return new Ceil(SemanticAnalyzer.getInstance().generateExpressionTree(
+					tokens));
+		}
+}
---
+++ src/picasso/parser/language/expressions/Ceil.java
@@ -0,0 +1,56 @@
+package picasso.parser.language.expressions;
+
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the ceiling function in the Picasso language
+ * 
+ * @author calebchoe
+ *
+ */
+public class Ceil extends UnaryFunction {
+
+	/**
+	 * Create a ceiling expression that takes as a parameter the given expression
+	 * 
+	 * @param param the expression to ceil
+	 */
+	public Ceil(ExpressionTreeNode param) {
+		super(param);
+	}
+
+	/**
+	 * Evaluates this expression at the given x,y point by evaluating the ceiling of
+	 * the function's parameter.
+	 * 
+	 * @return the color from evaluating the ceiling of the expression's parameter
+	 */
+
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = Math.ceil(result.getRed());
+		double green = Math.ceil(result.getGreen());
+		double blue = Math.ceil(result.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof Ceil)) {
+			return false;
+		}
+		Ceil c = (Ceil) obj;
+		return param.equals(c.param);
+	}
+
+}
