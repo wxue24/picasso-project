42191a1f385e38ca108a8202e127b8b8d94c13c8
wxue <wxue@williams-mbp.ad.wlu.edu>
2021-12-03 11:55:42 -0500

    Added EvaluatorTests for floor and abs

--- src/tests/EvaluatorTests.java
+++ src/tests/EvaluatorTests.java
@@ -47,6 +47,58 @@ public class EvaluatorTests {
 		}
 	}
 
+	@Test
+	public void testFloorEvaluation() {
+		Floor myTree = new Floor(new X());
+
+		// some straightforward tests
+		assertEquals(new RGBColor(0, 0, 0), myTree.evaluate(.4, -1));
+		assertEquals(new RGBColor(0, 0, 0), myTree.evaluate(.999, -1));
+		assertEquals(new RGBColor(-1, -1, -1), myTree.evaluate(-.7, -1));
+
+		// test the ints; remember that y's value doesn't matter
+		for (int i = -1; i <= 1; i++) {
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, i));
+		}
+
+		double[] tests = { -.7, -.00001, .000001, .5 };
+
+		for (double testVal : tests) {
+			double floorOfTestVal = Math.floor(testVal);
+			assertEquals(new RGBColor(floorOfTestVal, floorOfTestVal, floorOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(floorOfTestVal, floorOfTestVal, floorOfTestVal),
+					myTree.evaluate(testVal, testVal));
+		}
+
+	}
+
+	@Test
+	public void testAbsEvaluation() {
+		Abs myTree = new Abs(new X());
+
+		// some straightforward tests
+		assertEquals(new RGBColor(.4, .4, .4), myTree.evaluate(.4, -1));
+		assertEquals(new RGBColor(0.999, .999, .999), myTree.evaluate(.999, -1));
+		assertEquals(new RGBColor(.7, .7, .7), myTree.evaluate(-.7, -1));
+
+		// test the ints; remember that y's value doesn't matter
+		for (int i = -1; i <= 1; i++) {
+			int expected = Math.abs(i);
+			assertEquals(new RGBColor(expected, expected, expected), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(expected, expected, expected), myTree.evaluate(i, i));
+		}
+
+		double[] tests = { -.7, -.00001, .000001, .5 };
+
+		for (double testVal : tests) {
+			double AbsOfTestVal = Math.abs(testVal);
+			assertEquals(new RGBColor(AbsOfTestVal, AbsOfTestVal, AbsOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(AbsOfTestVal, AbsOfTestVal, AbsOfTestVal), myTree.evaluate(testVal, testVal));
+		}
+
+	}
+
 	// TODO: More tests of evaluation
 
 }
