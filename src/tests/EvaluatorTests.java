02be21b791cfec13cffab191e2256af37fd2b5fd
bassic21 <bassic21@mail.wlu.edu>
2021-12-03 11:53:08 -0500

    Evaluator test for tan function

--- src/tests/EvaluatorTests.java
+++ src/tests/EvaluatorTests.java
@@ -38,6 +38,31 @@ public class EvaluatorTests {
 			assertEquals(new RGBColor(1, -1, 1), e.evaluate(i, i));
 		}
 	}
+	
+	@Test
+	public void testTanEvaluation() {
+		Tan myTree = new Tan(new X());
+		
+		// some straightforward tests
+		assertEquals(new RGBColor(Math.tan(.4), Math.tan(.4), Math.tan(.4)), myTree.evaluate(.4, -1));
+		assertEquals(new RGBColor(Math.tan(.999), Math.tan(.999), Math.tan(.999)), myTree.evaluate(.999, -1));
+		assertEquals(new RGBColor(Math.tan(-.7), Math.tan(-.7), Math.tan(-.7)), myTree.evaluate(-.7, -1));
+		
+		// test the ints; remember that y's value doesn't matter
+		for (int i = -1; i <= 1; i++) {
+			assertEquals(new RGBColor(Math.tan(i), Math.tan(i), Math.tan(i)), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(Math.tan(i), Math.tan(i), Math.tan(i)), myTree.evaluate(i, i));
+		}
+		
+		double[] tests = {-.7, -.00001, .000001, .5};
+		
+		for( double testVal : tests) {
+			double tanOfTestVal = Math.tan(testVal);
+			assertEquals(new RGBColor(tanOfTestVal, tanOfTestVal, tanOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(tanOfTestVal, tanOfTestVal, tanOfTestVal), myTree.evaluate(testVal, testVal));
+		}
+		
+	}
 
 	@Test
 	public void testXEvaluation() {
