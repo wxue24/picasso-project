010cdde40bd5ceead3fd7fa229aa031b5202e541
choec23 <choec23@mail.wlu.edu>
2021-12-03 11:46:48 -0500

    Added Ceil evaluation test

--- src/tests/EvaluatorTests.java
+++ src/tests/EvaluatorTests.java
@@ -47,6 +47,30 @@ public class EvaluatorTests {
 		}
 	}
 
-	// TODO: More tests of evaluation
+	@Test
+	public void testCeilEvaluation() {
+		Ceil myTree = new Ceil(new X());
+		
+		
+		assertEquals(new RGBColor(1, 1, 1), myTree.evaluate(.4, -1));
+		assertEquals(new RGBColor(1, 1, 1), myTree.evaluate(.999, -1));
+		assertEquals(new RGBColor(0, 0, 0), myTree.evaluate(-.7, -1));
+		
+		// test the ints; remember that y's value doesn't matter
+		for (int i = -1; i <= 1; i++) {
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, i));
+		}
+		
+		double[] tests = {-.7, -.00001, .000001, .5};
+		
+		for( double testVal : tests) {
+			double ceilOfTestVal = Math.ceil(testVal);
+			assertEquals(new RGBColor(ceilOfTestVal, ceilOfTestVal, ceilOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(ceilOfTestVal, ceilOfTestVal, ceilOfTestVal), myTree.evaluate(testVal, testVal));
+		}
+		
+	}
+	
 
 }
