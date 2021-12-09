6f630da0dd8443e441e80693da904b62c7be8454
choec23 <choec23@mail.wlu.edu>
2021-12-08 19:59:13 -0500

    Created the wrap function

--- src/picasso/parser/language/expressions/UnaryFunctions/Wrap.java
+++ src/picasso/parser/language/expressions/UnaryFunctions/Wrap.java
@@ -5,7 +5,6 @@ import picasso.parser.language.ExpressionTreeNode;
 import picasso.parser.language.expressions.RGBColor;
 import picasso.parser.language.expressions.UnaryFunction;
 
-
 /**
  * Represents the wrap function in the Picasso language
  * 
@@ -24,6 +23,42 @@ public class Wrap extends UnaryFunction {
 	public Wrap(ExpressionTreeNode param) {
 		super(param);
 	}
+	
+	/**
+	 * Creates a wrapvalue method that determines whether to continuously add 
+	 * or subtract 2 to value until it reaches the given range.
+	 * 
+	 * @param value 
+	 * 
+	 * @return returns the given result from wrapping the value
+	 */
+
+	public static double wrapvalue(double value) {
+		double remainder = 0;
+		double quotient;
+		double result = 0;
+
+		if (value > 1) {
+			while (value > 1) {
+				value -= 2;
+
+			}
+			remainder = value % 1;
+
+		}
+		if (value < -1) {
+			while (value < -1) {
+				value += 2;
+			}
+			remainder = value % 1;
+			result = ((value));
+
+		} else {
+			result = value;
+		}
+		return result;
+
+	}
 
 	/**
 	 * Evaluates this expression at the given x,y point by evaluating the wrap of
@@ -34,11 +69,10 @@ public class Wrap extends UnaryFunction {
 
 	@Override
 	public RGBColor evaluate(double x, double y) {
-		// need to wrap somehow, Math.wrap does not exist
 		RGBColor result = param.evaluate(x, y);
-		double red = (result.getRed());
-		double green = (result.getGreen());
-		double blue = (result.getBlue());
+		double red = wrapvalue(result.getRed());
+		double green = wrapvalue(result.getGreen());
+		double blue = wrapvalue(result.getBlue());
 
 		return new RGBColor(red, green, blue);
 	}
