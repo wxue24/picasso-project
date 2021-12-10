afcd674985bb57619c35f076f69de7312e543d50
wxue <wxue@williams-mbp.ad.wlu.edu>
2021-12-10 00:52:07 -0500

    Implemented Imagewrap function

---
+++ src/picasso/parser/tokens/StringToken.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package picasso.parser.tokens;
+
+/**
+ * Represents a string token
+ * 
+ * @author wxue
+ *
+ */
+public class StringToken extends Token {
+
+	private String s;
+
+	public StringToken(String s) {
+		super("String token");
+		this.s = s;
+	}
+
+	/**
+	 * 
+	 * @return string
+	 */
+	public String getValue() {
+		return s;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ": " + s;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (!(o instanceof StringToken)) {
+			return false;
+		}
+		StringToken other = (StringToken) o;
+		return s.equals(other.s);
+	}
+
+	@Override
+	public boolean isConstant() {
+		return false;
+	}
+
+	@Override
+	public boolean isFunction() {
+		return false;
+	}
+
+}
---
+++ src/picasso/parser/tokens/functions/ImagewrapToken.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the ImageWrap function token
+ *
+ */
+public class ImagewrapToken extends FunctionToken {
+
+	public ImagewrapToken() {
+		super("Image Wrap function token");
+	}
+
+}
