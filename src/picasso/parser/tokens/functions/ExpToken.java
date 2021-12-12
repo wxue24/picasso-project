69c819c6287fbdeeaafe53bcdb945f24f5b3c174
choec23 <choec23@mail.wlu.edu>
2021-12-12 13:27:30 -0500

    Created the exp functions

---
+++ src/picasso/parser/tokens/functions/ExpToken.java
@@ -0,0 +1,16 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the exp token function
+ * 
+ * @author calebchoe
+ *
+ */
+
+public class ExpToken extends FunctionToken {
+
+	public ExpToken() {
+		super("Exp Token Function");
+	}
+
+}
