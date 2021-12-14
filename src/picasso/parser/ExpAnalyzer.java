74d8537f8a1322bdc1e16a477f98ece038880ba7
bassic21 <bassic21@mail.wlu.edu>
2021-12-14 16:09:31 -0500

    Fix a few errors from the commit

--- src/picasso/parser/ExpAnalyzer.java
+++ src/picasso/parser/ExpAnalyzer.java
@@ -7,25 +7,6 @@ import picasso.parser.language.expressions.UnaryFunctions.Exp;
 import picasso.parser.language.expressions.UnaryFunctions.Floor;
 import picasso.parser.tokens.Token;
 
-public class ExpAnalyzer extends UnaryFunctionAnalyzer {
-
-
-	@Override
-	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Need to remove the floor token
-		// the parameter is the next token on the stack.
-		// But, it needs to be processed
-		return new Exp(SemanticAnalyzer.getInstance().generateExpressionTree(
-				tokens));
-	}
-
-}
-
-import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.UnaryFunctions.Exp;
-import picasso.parser.tokens.Token;
-
-
 /**
  * 
  * @author calebchoe
