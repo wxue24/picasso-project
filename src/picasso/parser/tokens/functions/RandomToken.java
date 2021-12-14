48533f436279bb8a8e162b2e288c56c46c89d35a
bassic21 <bassic21@mail.wlu.edu>
2021-12-14 18:12:16 -0500

    Implemented random function

---
+++ src/picasso/parser/tokens/functions/RandomToken.java
@@ -0,0 +1,9 @@
+package picasso.parser.tokens.functions;
+
+import picasso.parser.tokens.Token;
+
+public class RandomToken extends FunctionToken {
+	public RandomToken() {
+		super("Random Color function token");
+	}
+}
