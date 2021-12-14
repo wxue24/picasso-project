9901a8ccb297ec757b44fa28158eeebe55e2ffd8
bassic21 <bassic21@mail.wlu.edu>
2021-12-14 13:23:37 -0500

    Implemented ImageClip function

---
+++ src/picasso/parser/tokens/functions/ImageClipToken.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the ImageWrap function token
+ *
+ */
+public class ImageClipToken extends FunctionToken {
+
+	public ImageClipToken() {
+		super("Image Clip function token");
+	}
+
+}
