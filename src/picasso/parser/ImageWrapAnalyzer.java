bbd1b1cc6d4c5269fcfafeb84d45bc86b60ac225
bassic21 <bassic21@mail.wlu.edu>
2021-12-14 13:23:09 -0500

    Implemented imageWrap to use any img in Image folder

--- src/picasso/parser/ImageWrapAnalyzer.java
+++ src/picasso/parser/ImageWrapAnalyzer.java
@@ -6,7 +6,7 @@ package picasso.parser;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Imagewrap;
+import picasso.parser.language.expressions.MultiArgumentFunctions.ImageWrap;
 import picasso.parser.tokens.StringToken;
 import picasso.parser.tokens.Token;
 
@@ -14,13 +14,14 @@ import picasso.parser.tokens.Token;
  * Handles parsing the ImageWrap function
  * 
  * @author wxue
- *
+ * 
  */
-public class ImagewrapAnalyzer implements SemanticAnalyzerInterface {
 
+public class ImageWrapAnalyzer implements SemanticAnalyzerInterface {
+ 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		// Pop Imagewrap token
+		// Pop ImageWrap token
 		tokens.pop();
 
 		ExpressionTreeNode rightExp = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
@@ -30,7 +31,7 @@ public class ImagewrapAnalyzer implements SemanticAnalyzerInterface {
 		StringToken token = (StringToken) tokens.pop();
 		String name = token.getValue();
 
-		return new Imagewrap(name, leftExp, rightExp);
+		return new ImageWrap(name, leftExp, rightExp);
 	}
 
 }
---
+++ src/picasso/parser/language/expressions/MultiArgumentFunctions/ImageWrap.java
@@ -0,0 +1,68 @@
+package picasso.parser.language.expressions.MultiArgumentFunctions;
+
+import java.awt.Color;
+import java.util.Arrays;
+
+import picasso.model.Pixmap;
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.MultiArgumentFunction;
+import picasso.parser.language.expressions.RGBColor;
+import picasso.parser.language.expressions.UnaryFunctions.Wrap;
+
+/**
+ * Represents the imageWrap function in the Picasso language
+ * 
+ * 
+ * @author cbassi
+ *
+ */
+public class ImageWrap extends ImageFunctions {
+
+	/**
+	 * Create an ImageWrap expression that takes a list of parameters.
+	 * 
+	 * @param param the expression to ImageWrap
+	 */
+
+	public ImageWrap(String name, ExpressionTreeNode xcoordexp, ExpressionTreeNode ycoordexp) {
+		super(name, xcoordexp,ycoordexp);
+		
+	}
+
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		// Evaluate expression
+		RGBColor xcoord = xcoordexp.evaluate(x, y);
+		RGBColor ycoord = ycoordexp.evaluate(x, y);
+		
+		double xcoordB = Wrap.wrapvalue(xcoord.getBlue());
+		double ycoordB = Wrap.wrapvalue(ycoord.getBlue());
+
+		// Get color from original image at these coordinates
+		int[] scaledCoords = scaleCoords(xcoordB, ycoordB);
+		RGBColor originalColor = new RGBColor(new Color(image.getRGB(scaledCoords[0], scaledCoords[1])));
+		return originalColor;
+	}
+
+	/**
+	 * Returns coordinates of image corresponding to coordinates
+	 * 
+	 * @param x - xcoord in range [-1,1]
+	 * @param y - ycoord in range [-1,1]
+	 * @return scaled to coordinates as an integer array, with index at 0 as
+	 *         the x-coord and 1 as the y-coord
+	 */
+	private int[] scaleCoords(double x, double y) {
+		// Set range to [0, 2]
+		x = x + 1;
+		y = y + 1;
+
+		// Get ratio
+		double xratio = x / 2;
+		double yratio = y / 2;
+
+		// Get scaled up coordinates
+		return new int[] { (int) (xratio * (image.getWidth()-1)), (int) (yratio * (image.getHeight()-1))};
+	}
+
+}
