ac783b1b04ad28684912d5afaa8cc1a783522764
bassic21 <bassic21@mail.wlu.edu>
2021-12-14 13:47:37 -0500

    Implemented PerlinBW Function

---
+++ src/picasso/parser/tokens/functions/PerlinBWToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+public class PerlinBWToken extends FunctionToken {
+	/**
+	 * Represents the PerlinColor function token
+	 *
+	 */ 
+	
+	public PerlinBWToken() {
+		super("Perlin Black and White function token");
+	}
+
+}
